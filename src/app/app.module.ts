eac3288480a62d4fed116ab4150ab77c9f99477d
bharner <robert.d.harner.ctr@mail.mil>
2016-12-19 16:07:22 -0500

    Minor code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
